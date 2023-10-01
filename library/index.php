08cb8c14e1dfd523876a3eb2855b3a1cb5801f65
abdulrahman1551 <ab.binzaila@gmail.com>
2023-10-01 19:48:05 +0300

    version one of the library website
    1-the Database is not working for some reason.
    2- the design is shit.
    3- there are some issues with the authentication like the year input accept 6 digits.

--- library/index.php
+++ library/index.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection settings
-$host = "localhost";
-$username = "your_username";
-$password = "your_password";
-$database = "your_database";
+$host = "127.0.0.1";
+$username = "root";
+$password = '';
+$database = "librarydb";
 
 // Create a connection
 $conn = new mysqli($host, $username, $password, $database);
@@ -14,13 +14,13 @@ if ($conn->connect_error) {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Add a new book
-    if (isset($_POST['bookName']) && isset($_POST['authorName']) && isset($_POST['bookNumber']) && isset($_POST['publishingDate'])) {
-        $bookName = $_POST['bookName'];
-        $authorName = $_POST['authorName'];
-        $bookNumber = $_POST['bookNumber'];
-        $publishingDate = $_POST['publishingDate'];
+    if (isset($_POST['name']) && isset($_POST['number']) && isset($_POST['author']) && isset($_POST['publishDate'])) {
+        $bookName = $_POST['name'];
+        $authorName = $_POST['author'];
+        $bookNumber = $_POST['number'];
+        $publishingDate = $_POST['publishDate'];
 
-        $sql = "INSERT INTO books (book_name, author_name, book_number, publishing_date) VALUES ('$bookName', '$authorName', $bookNumber, '$publishingDate')";
+        $sql = "INSERT INTO book (name ,author,number,publishDate) VALUES ('$bookName', '$authorName', $bookNumber, '$publishingDate')";
         if ($conn->query($sql) === TRUE) {
             echo "Book added successfully";
         } else {
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 // Retrieve books from the database
 $books = array();
-$result = $conn->query("SELECT * FROM books");
+$result = $conn->query("SELECT * FROM book");
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $books[] = $row;
@@ -41,49 +41,39 @@ if ($result->num_rows > 0) {
 $conn->close();
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-wA5MQ0lw9lqRuGwU2DS5fejqgU6hx4u3bmkj/3/KtZc6zERgXLbY6zvccYlHO7sS" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
+    
+    
     <title>Library</title>
 </head>
 <body>
     <div class="container">
-        <h1 class="display-4">Library</h1>
+        <h1>Library</h1>
         
         <!-- Add a Book Form -->
         <h2>Add a Book</h2>
         <form id="add-book-form">
-            <div class="form-group">
-                <input type="text" class="form-control" id="book-name" placeholder="Book Name" required>
-            </div>
-            <div class="form-group">
-                <input type="text" class="form-control" id="author-name" placeholder="Author Name" required>
-            </div>
-            <div class="form-group">
-                <input type="number" class="form-control" id="book-number" placeholder="Book Number" required>
-            </div>
-            <div class="form-group">
-                <input type="date" class="form-control" id="publishing-date" required>
-            </div>
-            <button type="button" class="btn btn-primary" id="add-book-button">Add Book</button>
+            <input type="text" id="book-name" placeholder="Book Name" required>
+            <input type="text" id="author-name" placeholder="Author Name" required>
+            <input type="number" id="book-number" placeholder="Book Number" required>
+            <input type="date" id="publishing-date" required>
+            <button type="button" id="add-book-button" required>Add Book</button>
         </form>
 
         <!-- Search for Books -->
         <h2>Search for Books</h2>
-        <div class="input-group">
-            <input type="text" class="form-control" id="search-query" placeholder="Search">
-            <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button" id="search-button">Search</button>
-            </div>
-        </div>
+        <input type="text" id="search-query" placeholder="Search">
+        <button type="button" id="search-button">Search</button>
 
         <!-- Books Table -->
         <h2>Books</h2>
-        <table class="table">
+        <table>
             <thead>
                 <tr>
                     <th>Book Name</th>
@@ -97,7 +87,11 @@ $conn->close();
             </tbody>
         </table>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="app.js"></script>
 </body>
 </html>
+
+
+
 
